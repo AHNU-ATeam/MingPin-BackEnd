157ee1c5e48c999d35ce8ad608be5556f001b6a8
LittleHuo <3466691370@qq.com>
2024-08-05 23:28:01 +0800

    test

--- src/main/java/com/mingpin/Application.java
+++ src/main/java/com/mingpin/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 //        test
+
         System.out.println("Yes");
     }
 
