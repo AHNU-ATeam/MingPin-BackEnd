5109f1385805b8b0cfb33572e50fdaedf152192a
LittleHuo <3466691370@qq.com>
2024-08-05 23:57:55 +0800

    增加相关文件

---
+++ src/main/java/com/mingpin/mapper/RecipeMapper.java
@@ -0,0 +1,21 @@
+package com.mingpin.mapper;
+
+import com.mingpin.pojo.Recipe;
+import org.apache.ibatis.annotations.*;
+
+
+@Mapper
+public interface RecipeMapper {
+    @Insert("insert into recipe(content,date,campus_id,created_at,updated_at)"+
+            "values (#{content},#{date},#{campus_id},#{created_at},#{updated_at})")
+    void add(Recipe recipe);
+
+    @Delete("delete from recipe where id=#{recipeId}")
+    void deleteById(Integer recipeId);
+
+    @Update("update recipe set content = #{content}, campus_id = #{campus_id}, updated_at = #{updated_at} where recipe_id = #{recipe_id}")
+    void update(Recipe recipe);
+
+    @Select("select * from recipe where id = #{recipeId}")
+    Recipe findById(Integer recipeId);
+}
