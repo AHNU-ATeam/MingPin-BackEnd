e0c3dbcbdf06cda056fdb71763d63706cad0e47d
邢小羊 <coffeexing998@gmail.com>
2024-09-11 12:56:08 +0800

    增加前端上传文件至OSS的接口

--- core/src/main/java/com/chuanglian/mingpin/controller/OssController.java
+++ core/src/main/java/com/chuanglian/mingpin/controller/OssController.java
@@ -1,22 +1,30 @@
 package com.chuanglian.mingpin.controller;
 
 import com.chuanglian.mingpin.pojo.Result;
+import com.chuanglian.mingpin.service.OssService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
-public class OSSController {
+public class OssController {
+
+    @Autowired
+    private OssService ossService;
 
     @PostMapping("/oss")
     public Result<String> getOssUrl(
             @RequestParam("file") MultipartFile file,
-            @RequestParam(value = "type", required = false) String type) {
+            @RequestParam(value = "type", required = false) String type) throws IOException {
 
 
-        return Result.success();
+        // TODO 省略验证逻辑
+
+        return ossService.getOssUrl(file);
     }
 
 }
