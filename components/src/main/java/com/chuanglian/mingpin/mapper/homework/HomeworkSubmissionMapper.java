b48c60d10b856550f947ade3f72a3ff6e0b0e9ee
XingClay 邢小羊 <coffeexing998@gmail.com>
2024-09-19 22:21:29 +0800

    Revert "Review.huang ln"

--- components/src/main/java/com/chuanglian/mingpin/mapper/homework/HomeworkSubmissionMapper.java
+++
@@ -1,9 +0,0 @@
-package com.chuanglian.mingpin.mapper.homework;
-
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.chuanglian.mingpin.entity.homework.HomeworkSubmission;
-
-@TableName("homeworkManagement.homework_submission")
-public interface HomeworkSubmissionMapper extends BaseMapper<HomeworkSubmission> {
-}
--- components/src/main/java/com/chuanglian/mingpin/mapper/recipe/RecipeMapper.java
+++ components/src/main/java/com/chuanglian/mingpin/mapper/recipe/RecipeMapper.java
@@ -11,4 +11,6 @@ import java.util.List;
 @TableName("recipeManagement.recipe")
 public interface RecipeMapper extends BaseMapper<Recipe> {
 
+    @Select("SELECT * FROM recipeManagement.recipe WHERE date = #{date} and type = #{type}")
+    List<Recipe> selectByDate(String date, int type);
 }
