d8e56b1d07cc5be13775a207e8af1b9dafc7904f
邢小羊 <xingclay@163.com>
2024-09-27 17:14:32 +0800

    修改无法运行的代码

--- attendance/src/main/java/com/chuanglian/mingpin/service/AttendanceService.java
+++ attendance/src/main/java/com/chuanglian/mingpin/service/AttendanceService.java
@@ -1,28 +1,28 @@
-package com.chuanglian.mingpin.service;
-
-import com.chuanglian.mingpin.entity.attendance.EmployeeAttendance;
-import com.chuanglian.mingpin.entity.attendance.StudentAttendance;
-
-import java.util.List;
-import java.util.Map;
-
-public interface AttendanceService {
-
-    List<Map<String, Object>> getClassAttendance(Integer id,String date);
-
-    Map<Integer, List<StudentAttendance>> getStudAttendance(Integer id, String date);
-
-
-    Integer stuAttendance(Integer studentAttendance);
-
-    Integer stuUnattendance(Integer studentAttendance);
-
-    Integer stuAskForLeave(Integer studentAttendance);
-
-    Integer StuSignOut(Integer studentAttendance);
-
-    List<EmployeeAttendance> getTeaAttendance(Integer id, String date);
-
-
-    Integer setTeaAttendance(Integer id, Integer type, String location, String photo);
-}
+//package com.chuanglian.mingpin.service;
+//
+//import com.chuanglian.mingpin.entity.attendance.EmployeeAttendance;
+//import com.chuanglian.mingpin.entity.attendance.StudentAttendance;
+//
+//import java.util.List;
+//import java.util.Map;
+//
+//public interface AttendanceService {
+//
+//    List<Map<String, Object>> getClassAttendance(Integer id,String date);
+//
+//    Map<Integer, List<StudentAttendance>> getStudAttendance(Integer id, String date);
+//
+//
+//    Integer stuAttendance(Integer studentAttendance);
+//
+//    Integer stuUnattendance(Integer studentAttendance);
+//
+//    Integer stuAskForLeave(Integer studentAttendance);
+//
+//    Integer StuSignOut(Integer studentAttendance);
+//
+//    List<EmployeeAttendance> getTeaAttendance(Integer id, String date);
+//
+//
+//    Integer setTeaAttendance(Integer id, Integer type, String location, String photo);
+//}
